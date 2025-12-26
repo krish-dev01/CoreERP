6463ad659014186060057fbb7cd99e345cbe7918
Krishnaraj <krishdev10072k@gmail.com>
2025-12-27 02:13:50 +0530

    CoreERP Spring Boot application running successfully

---
+++ src/main/java/com/coreerp/coreerp/CoreErpApplication.java
@@ -0,0 +1,13 @@
+package com.coreerp.coreerp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoreErpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoreErpApplication.class, args);
+	}
+
+}
